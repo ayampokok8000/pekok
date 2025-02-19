e17b79bab3d26174f7253bb791215e63d3e5e190
ayampokok8000 <kumamotopower1@gmail.com>
2025-02-19 11:52:52 +0700

    server.js

--- server.js
+++ server.js
@@ -21,29 +21,22 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-request-start',
-    'x-request-id',
-    'via',
-    'connect-time',
-    'total-route-time',
-    // Other Heroku added debug headers
-    // 'x-forwarded-for',
-    // 'x-forwarded-proto',
-    // 'x-forwarded-port',
-  ],
-  redirectSameOrigin: true,
-  httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: [
+        'cookie',
+        'cookie2',
+        'x-request-start',
+        'x-request-id',
+        'via',
+        'connect-time',
+        'total-route-time',
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+        xfwd: false,
+    },
 }).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
